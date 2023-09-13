f47ab74cb4283dc3acfd960955c872e4a6fa9c4f
kwonhd <130338461+kwonhd@users.noreply.github.com>
2023-09-13 11:22:49 +0900

    mlmid1

--- ML/mlbasic.py
+++ ML/mlbasic.py
@@ -48,12 +48,34 @@ X_train,X_test,Y_train,Y_test=train_test_split(irdata,irtgt,test_size=0.3,shuffl
 print(X_train.shape,X_test.shape)#%%train과 test 잘 분리되었는지 확인
 print(Y_train.shape,Y_test.shape)#%%
 
-#%%
-knn3 = KNeighborsClassifier(n_neighbors=3) # 모델지정 :근처 3개를 기준으로 분류
-knn3.fit(X_train,Y_train) # 학습시키기
-pred = knn3.predict(X_test) # 시험보기
-print(pred) # 시험본 답 출력
-print(Y_test) # 실제 답 출력
-# %%
-acc = accuracy_score(pred,Y_test) # 답지랑 비교해서 맞는지 확인
-print('점수 :',acc)
+#%% 하이퍼 파이미터 튜닝 KNN
+for i in range(3,20,2):
+    print('knn:',i)
+    knn3 = KNeighborsClassifier(n_neighbors=i) # 모델지정 :근처 3개를 기준으로 분류
+    knn3.fit(X_train,Y_train) # 학습시키기
+    pred = knn3.predict(X_test) # 시험보기
+    print(pred) # 시험본 답 출력
+    print(Y_test) # 실제 답 출력
+    acc = accuracy_score(pred,Y_test) # 답지랑 비교해서 맞는지 확인
+    print('점수[',i,'] :',acc)
+#%% SVM
+from sklearn.svm import SVC
+for i in range(1,10):
+    svc=SVC(C=i)
+    svc.fit(X_train,Y_train)
+    pred=svc.predict(X_test)
+    print(pred)
+    print(Y_test)
+    acc=accuracy_score(pred,Y_test)
+    print('SVM [',i,'] acc:',acc)
+# %% 디시젼트리
+from sklearn.tree import DecisionTreeClassifier as DT
+for j in range(2,10):
+    for i in range(2,10):
+        dt=DT(max_depth=i,min_samples_leaf=j)
+        dt.fit(X_train,Y_train)
+        pred = dt.predict(X_test)
+        print(pred)
+        print(Y_test)
+        acc=accuracy_score(pred,Y_test)
+        print('DT[',i,',',j,'] acc:',acc)
