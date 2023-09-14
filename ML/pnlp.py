68d929a5b7f70f8fad02db3926717b933667426c
kwonhd <130338461+kwonhd@users.noreply.github.com>
2023-09-14 11:50:48 +0900

    automl

---
+++ ML/pnlp.py
@@ -0,0 +1,9 @@
+#%%
+import sqlite3 #sqlite3 데이터베이스
+import re #정규식
+import numpy as np #숫자 
+import pandas as pd #데이터 처리  
+import matplotlib.pyplot as plt #그래프 
+import matplotlib 
+import seaborn as sns #그래프 고도화
+# %%
